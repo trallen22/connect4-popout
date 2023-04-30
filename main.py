60ecb8d27650371b4e470a84509252a15d0071b2
trallen22 <77846813+trallen22@users.noreply.github.com>
2023-04-30 17:18:18 -0400

    user input for which algorithm to use

--- main.py
+++ main.py
@@ -22,7 +22,18 @@ AI_PIECE = 2
 play = 1
 curBoard = Board(ROWS, COLS)
 
-turn = random.choice([0, 1])
+turn = random.choice([PLAYER_TURN, AI_TURN])
+
+chooseAI = 1
+while chooseAI:
+    mindset = input('which algorithm to play: minimax(a) or mcts(b)  ')
+    if mindset.lower() == 'minimax' or mindset.lower() == 'a':
+        chooseAI = 0
+    elif mindset.lower() == 'mcts' or mindset.lower() == 'b':
+        chooseAI = 0
+    else:
+        print(f'{mindset} is an invalid algorithm')
+        mindset = input('which algorithm to play: minimax(a) or mcts(b)  ')
 
 os.system('clear')
 print(curBoard)
@@ -60,29 +71,29 @@ while play:
                 turn = AI_TURN
 
     # MINIMAX
-    # elif turn == AI_TURN:
-    #     col, value, action = minimax(
-    #         curBoard, 2, -math.inf, math.inf, True)
-    #     if action == 0: # drop 
-    #         curBoard.drop_piece(col, AI_PIECE)
-    #     elif action == 1: # pop
-    #         curBoard.popout_piece(col)
-    #     else:
-    #         print('something went wrong')
-    #     #os.system('clear')
-    #     print(curBoard)
-    #     print(f'last move: action = {action} col = {col}')
-    #     if winning_move(curBoard, AI_PIECE):
-    #         print("CONGRATS COMPUTER")
-    #         play = 0
-    #     elif winning_move(curBoard, PLAYER_PIECE):
-    #         print("CONGRATS PLAYER")
-    #         play = 0
-    #     else: 
-    #         turn = PLAYER_TURN
+    elif turn == AI_TURN and (mindset == 'minimax' or mindset == 'a'):
+        col, value, action = minimax(
+            curBoard, 4, -math.inf, math.inf, True)
+        if action == 0: # drop 
+            curBoard.drop_piece(col, AI_PIECE)
+        elif action == 1: # pop
+            curBoard.popout_piece(col)
+        else:
+            print('something went wrong')
+        os.system('clear')
+        print(curBoard)
+        print(f'last move: action = {action} col = {col}')
+        if winning_move(curBoard, AI_PIECE):
+            print("CONGRATS COMPUTER")
+            play = 0
+        elif winning_move(curBoard, PLAYER_PIECE):
+            print("CONGRATS PLAYER")
+            play = 0
+        else: 
+            turn = PLAYER_TURN
 
     # MCTS 
-    elif turn == AI_TURN:
+    elif turn == AI_TURN and (mindset == 'mcts' or mindset == 'b'):
         nextMove = mcts(curBoard, AI_PIECE, PLAYER_PIECE)
         if nextMove[1] == 0: # drop 
             curBoard.drop_piece(nextMove[0], AI_PIECE)
